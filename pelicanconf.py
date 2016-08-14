fe3a6a7556de8f9553f048962c590c63a792d0d3
Liung <lc.pypi@gmail.com>
2016-08-14 18:54:52 +0800

    use the pelican plugin "render_math" and update blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -82,7 +82,8 @@ NOTEBOOK_DIR = 'downloads/notebooks'
 
 # plugin config
 PLUGIN_PATHS = ['pelican-plugins/']
-PLUGINS = ['summary', 'liquid_tags.img', 'liquid_tags.video',
+PLUGINS = ['summary', 'render_math',
+           'liquid_tags.img', 'liquid_tags.video',
            'liquid_tags.include_code',  # 'liquid_tags.notebook',
            'liquid_tags.literal']
 
